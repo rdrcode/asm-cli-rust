8139eda477084b9b31c9add8ab03bd7cdbee6b62
rdrcode <rdrcode@gmx.eu>
2021-08-05 00:13:14 +0200

    Implemented command parameters parser and related unit tests

--- src/parser.rs
+++ src/parser.rs
@@ -9,6 +9,7 @@ use regex::RegexSet;
 use thiserror::Error;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
+use crate::machine::context::Register;
 
 
 /// ParseError enumerates all possible errors returned by this library.
@@ -31,6 +32,10 @@ pub enum ParseError {
     #[error("Unknown command error")]
     UnknownCommandError,
 
+    /// Represents a general parameter error.
+    #[error("Parameter error")]
+    ParameterError,
+
     /// Represents a failure to parse an integer constant.
     #[error("Parse integer error")]
     ParseIntError,
@@ -46,7 +51,7 @@ pub enum ParseError {
 }
 
 
-#[derive(Debug,EnumIter)]
+#[derive(Copy,Clone,PartialEq,Debug,EnumIter)]
 pub enum Command {
     Quit,
     Exit,
@@ -65,6 +70,13 @@ impl fmt::Display for Command {
     }
 }
 
+#[derive(Clone,PartialEq,Debug)]
+pub enum Parameter<T> {
+    Register(Register),
+    Integer(T),
+    Identifier(String),
+    Assign,
+}
 
 #[derive(Debug)]
 pub struct Parser<T> {
@@ -73,6 +85,7 @@ pub struct Parser<T> {
     regexset:   RegexSet,
     delim:      HashSet<u8>,
     values:     Vec<T>,
+    reg_names:  HashSet<Register>,
 }
 
 
@@ -95,31 +108,37 @@ where
                 // The regexes ignore out-of-range digits in binary and
                 // octal constant. Such errors will be handled by the
                 // string to integer conversion
-                r"^\$[a-zA-Z][0-9a-zA-Z_]*$", //  0
-                r"^0x[0-9a-fA-F]+$",          //  1
-                r"^0o[0-9]+$",                //  2
-                r"^0b[0-9]+$",                //  3
-                r"^0d\d+$",                   //  4
-                r"^[0-9]+o$",                 //  5
-                r"^[0-9]+b$",                 //  6
-                r"^\d+d$",                    //  7
-                r"^\d+$",                     //  8
-                r"^\$\d+$",                   //  9
-                r"^\$_+$",                    // 10
+                // Single digit numbers without prefix or suffix are not
+                // matched as int and herefore not converted to hex prefix
+                // (0x) representation for better readability
+                r"^\$[a-zA-Z][0-9a-zA-Z_]*$",       //  0
+                r"^0x[0-9a-fA-F]+$",                //  1
+                r"^0o[0-9]+$",                      //  2
+                r"^0b[0-9]+$",                      //  3
+                r"^0d\d+$",                         //  4
+                r"^[0-9]+o$",                       //  5
+                r"^[0-9]+b$",                       //  6
+                r"^\d+d$",                          //  7
+                r"^\d\d+$",                         //  8 (exclude single digit numbers)
+                r"^\$\d+$",                         //  9
+                r"^\$_+$",                          // 10
+                r"^\d+$",                           // 11 single digit numbers
+                r"^[a-zA-Z][0-9a-zA-Z_\.]*$",         // 12 Identifier
+                r"^_+[0-9a-zA-Z]+[0-9a-zA-Z_]*$",   // 13 Identifier
+                r#"^".+"$"#,                        // 14 Identifier
             ]).unwrap(),
             delim: "[]()+-*/%&^|<>, ".as_bytes()
                                      .iter()
                                      .cloned()
                                      .collect(),
             values: Vec::<T>::new(),
+            reg_names: HashSet::new(),
         }
     }
-
 
     fn from_constant(&self, src: &str) -> Result<T,ParseError> {
         self.constants.get(src).cloned().ok_or(ParseError::UndefinedConstError)
     }
-
 
     fn from_str_radix(src: &str, radix: u32) -> Result<T,ParseError> {
     //where T: num_traits::Num<FromStrRadixErr = std::num::ParseIntError> {
@@ -127,11 +146,9 @@ where
         T::from_str_radix(src, radix).map_err(|_| ParseError::ParseIntError)
     }
 
-
     pub fn add_value(&mut self, value: T) {
         self.values.push(value);
     }
-
 
     pub fn get_value(&self, index: usize) -> Result<T,ParseError> {
         if index < self.values.len() {
@@ -144,8 +161,15 @@ where
         }
     }
 
+    pub fn define_constant(&mut self, name: &str, value: T) {
+        self.constants.insert(name.to_owned(), value);
+    }
 
-    pub fn parse_cmd(&self, input: &str) -> Option<Result<&Command,ParseError>> {
+    pub fn parse_cmd(&self, input: &str) -> Option<Result<(Command,Vec<Parameter<T>>),ParseError>> {
+        let input = input.trim();
+        if input.len() == 0 {
+            return None;
+        }
         let v: Vec<&str> = input.split(' ').collect();
         if !v.is_empty() {
             let m: Vec<_> = self.commands.iter()
@@ -160,7 +184,17 @@ where
                         None
                     }
                 },
-                1 => Some(Ok(m[0])),
+                1 => {
+                    let mut parameters = vec![];
+                    for p in v.iter().skip(1) {
+                        let result = self.parse_parameter(p);
+                        if result.is_err() {
+                            return Some(Err(ParseError::ParameterError));
+                        }
+                        parameters.push(result.unwrap());
+                    }
+                    Some(Ok((*m[0],parameters)))
+                },
                 _ => Some(Err(ParseError::CommandError))
             }
         } else {
@@ -168,6 +202,52 @@ where
         }
     }
 
+    fn map_int(v: Result<T,ParseError>) -> Result<Parameter<T>,ParseError> {
+        if v.is_ok() {
+            Ok(Parameter::Integer(v.unwrap()))
+        } else {
+            Err(ParseError::ParameterError)
+        }
+    }
+
+    pub fn parse_parameter(&self, input: &str) -> Result<Parameter<T>,ParseError> {
+        if let Some(reg) = self.reg_names.get(&Register::from(input.to_uppercase().as_str())) {
+            return Ok(Parameter::Register(*reg));
+        } 
+        let matches: Vec<_> = self.regexset.matches(input).into_iter().collect();
+        if matches.is_empty() {
+            Err(ParseError::ParameterError)
+        } else {
+            let result = match matches[0] {
+                0  => Parser::map_int(self.from_constant(&input[1..])),
+                1  => Parser::map_int(Parser::from_str_radix(&input[2..], 16)),
+                2  => Parser::map_int(Parser::from_str_radix(&input[2..],  8)),
+                3  => Parser::map_int(Parser::from_str_radix(&input[2..],  2)),
+                4  => Parser::map_int(Parser::from_str_radix(&input[2..], 10)),
+                5  => Parser::map_int(Parser::from_str_radix(&input[..input.len()-1], 8)),
+                6  => Parser::map_int(Parser::from_str_radix(&input[..input.len()-1], 2)),
+                7  => Parser::map_int(Parser::from_str_radix(&input[..input.len()-1], 10)),
+                8|11 => Parser::map_int(Parser::from_str_radix(input, 10)),
+                9  => {
+                    let index = u32::from_str_radix(&input[1..], 10);
+                    if index.is_err() {
+                        return Err(ParseError::ParameterError);
+                    }
+                    Parser::map_int(self.get_value(index.unwrap() as usize))
+                },
+                10 => {
+                    match self.values.last() {
+                        Some(value) => Ok(Parameter::Integer(value.clone())),
+                        None        => return Err(ParseError::ParameterError),
+                    }
+                },
+                12|13 => Ok(Parameter::Identifier(input.to_string())),
+                14    => Ok(Parameter::Identifier(input[1..input.len()-1].to_string())),
+                _  => return Err(ParseError::ParameterError),
+            };
+            result
+        }
+    }
 
     pub fn parse_int(&self, input: &str) -> Option<Result<T,ParseError>> {
         let matches: Vec<_> = self.regexset.matches(input).into_iter().collect();
@@ -202,7 +282,6 @@ where
             Some(result)
         }
     }
-
 
     pub fn parse_asm(&self, input: &str) -> Result<String,ParseError> {
         let indices: Vec<_> = input.as_bytes()
@@ -239,45 +318,55 @@ where
         Ok(tokens.join(""))
     }
 
+    pub fn set_reg_names(&mut self, regs: &Vec<Register>) {
+        for reg in regs {
+            self.reg_names.insert(*reg);
+        }
+    }
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::machine::context::Context;
 
     #[test]
     fn test_value_history() {
         let mut parser = Parser::<u32>::new();
 
-        let value = parser.get_value(0);
-        assert_eq!(Err(ParseError::RangeError), value);
+        assert_eq!(parser.get_value(0), Err(ParseError::RangeError));
+        assert_eq!(parser.parse_int("$0"), Some(Err(ParseError::RangeError)));
+        assert_eq!(parser.parse_int("$_"), Some(Err(ParseError::RangeError)));
 
         parser.add_value(42);
-        let value = parser.get_value(0);
-        assert_eq!(Ok(42), value);
-        let value = parser.get_value(1);
-        assert_eq!(Err(ParseError::RangeError), value);
+        assert_eq!(parser.get_value(0), Ok(42));
+        assert_eq!(parser.get_value(1), Err(ParseError::RangeError));
+        assert_eq!(parser.parse_int("$0"), Some(Ok(42)));
+        assert_eq!(parser.parse_int("$1"), Some(Err(ParseError::RangeError)));
+        assert_eq!(parser.parse_int("$_"), Some(Ok(42)));
 
         parser.add_value(10);
-        let value = parser.get_value(0);
-        assert_eq!(Ok(42), value);
-        let value = parser.get_value(1);
-        assert_eq!(Ok(10), value);
-        let value = parser.get_value(2);
-        assert_eq!(Err(ParseError::RangeError), value);
+        assert_eq!(parser.get_value(0), Ok(42));
+        assert_eq!(parser.get_value(1), Ok(10));
+        assert_eq!(parser.get_value(2), Err(ParseError::RangeError));
+        assert_eq!(parser.parse_int("$0"), Some(Ok(42)));
+        assert_eq!(parser.parse_int("$1"), Some(Ok(10)));
+        assert_eq!(parser.parse_int("$2"), Some(Err(ParseError::RangeError)));
+        assert_eq!(parser.parse_int("$_"), Some(Ok(10)));
+    }
 
-        let result = parser.parse_int("$0");
-        assert_eq!(Some(Ok(42)), result);
+    #[test]
+    fn test_constants() {
+        let mut parser = Parser::<u32>::new();
 
-        let result = parser.parse_int("$1");
-        assert_eq!(Some(Ok(10)), result);
-
-        let result = parser.parse_int("$3");
-        assert_eq!(Some(Err(ParseError::RangeError)), result);
-
-        let result = parser.parse_int("$_");
-        assert_eq!(Some(Ok(10)), result);
+        assert_eq!(parser.parse_int("$num"), Some(Err(ParseError::UndefinedConstError)));
+        parser.define_constant("num", 1234);
+        assert_eq!(parser.parse_int("$num"), Some(Ok(1234)));
+        parser.define_constant("num", 0xffff);
+        assert_eq!(parser.parse_int("$num"), Some(Ok(0xffff)));
+        parser.define_constant("l_10", 0xabcd);
+        assert_eq!(parser.parse_int("$l_10"), Some(Ok(0xabcd)));
     }
 
     #[test]
@@ -285,7 +374,6 @@ mod tests {
         let parser = Parser::<u32>::new();
 
         let pairs: Vec<(&str, Result<u32,ParseError>)> = vec![
-            ("0",            Ok(0)),
             ("100",          Ok(100)),
             ("65535",        Ok(65535)),
             ("0d0",          Ok(0)),
@@ -297,8 +385,7 @@ mod tests {
         ];
 
         for pair in pairs {
-            let result = parser.parse_int(pair.0);
-            assert_eq!(Some(pair.1), result);
+            assert_eq!(parser.parse_int(pair.0), Some(pair.1));
         }
     }
 
@@ -316,8 +403,7 @@ mod tests {
         ];
 
         for pair in pairs {
-            let result = parser.parse_int(pair.0);
-            assert_eq!(Some(pair.1), result);
+            assert_eq!(parser.parse_int(pair.0), Some(pair.1));
         }
     }
 
@@ -336,8 +422,7 @@ mod tests {
         ];
 
         for pair in pairs {
-            let result = parser.parse_int(pair.0);
-            assert_eq!(Some(pair.1), result);
+            assert_eq!(parser.parse_int(pair.0), Some(pair.1));
         }
     }
 
@@ -351,16 +436,16 @@ mod tests {
         ];
 
         for pair in pairs {
-            let result = parser.parse_int(pair.0);
-            assert_eq!(Some(pair.1), result);
+            assert_eq!(parser.parse_int(pair.0), Some(pair.1));
         }
     }
 
     #[test]
-    fn test_parse_int_err() {
+    fn test_parse_int_misc() {
         let parser = Parser::<u32>::new();
 
         let pairs: Vec<(&str, Option<Result<u32,ParseError>>)> = vec![
+            ("0",            None),
             ("0d10x",        None),
             ("0x1000000gf",  None),
             ("0x100000000",  Some(Err(ParseError::ParseIntError))),
@@ -369,8 +454,67 @@ mod tests {
         ];
 
         for pair in pairs {
-            let result = parser.parse_int(pair.0);
-            assert_eq!(pair.1, result);
+            assert_eq!(parser.parse_int(pair.0), pair.1);
         }
     }
+
+    #[test]
+    fn test_parse_asm() {
+        let mut parser = Parser::<u32>::new();
+        parser.define_constant("offset", 0x10000);
+
+        let pairs: Vec<(&str, Result<String,ParseError>)> = vec![
+            ("mov eax,1000",                 Ok("mov eax,0x3e8".to_string())),
+            ("mov al,'a'",                   Ok("mov al,'a'".to_string())),
+            ("add ebx,0d1000",               Ok("add ebx,0x3e8".to_string())),
+            ("lea edi,[2*ebx+eax+10000]",    Ok("lea edi,[2*ebx+eax+0x2710]".to_string())),
+            ("lea esi,[8*eax+$offset]",      Ok("lea esi,[8*eax+0x10000]".to_string())),
+        ];
+
+        for pair in pairs {
+            assert_eq!(parser.parse_asm(pair.0), pair.1);
+        }
+    }
+
+    #[test]
+    fn test_parse_cmd() {
+        let mut parser = Parser::<u32>::new();
+        assert_eq!(parser.parse_cmd("").is_none(), true);
+        assert_eq!(parser.parse_cmd("no command").is_none(), true);
+        assert_eq!(parser.parse_cmd("mov eax,1000").is_none(), true);
+
+        parser.set_reg_names(&Context::regs_x86_32());
+        parser.define_constant("offset", 0x10000);
+
+        let pairs: Vec<(&str, Result<(Command,Vec<Parameter<u32>>),ParseError>)> = vec![
+            (".help",                 Ok((Command::Help,vec![]))),
+            (" .help",                Ok((Command::Help,vec![]))),
+            (".help ",                Ok((Command::Help,vec![]))),
+            (".save filename",        Ok((Command::Save,vec![Parameter::Identifier("filename".to_string())]))),
+            (".save filename.txt",    Ok((Command::Save,vec![Parameter::Identifier("filename.txt".to_string())]))),
+            (".save \"context.txt\"", Ok((Command::Save,vec![Parameter::Identifier("context.txt".to_string())]))),
+            (".print eax",            Ok((Command::Print,vec![Parameter::Register(Register::from("EAX"))]))),
+            (".print eax ebx ecx",    Ok((Command::Print,vec![
+                                        Parameter::Register(Register::from("EAX")),
+                                        Parameter::Register(Register::from("EBX")),
+                                        Parameter::Register(Register::from("ECX")),
+                                      ]))),
+            (".print $offset",        Ok((Command::Print,vec![Parameter::Integer(0x10000)]))),
+            (".set label 0x1000",     Ok((Command::Set,vec![
+                                        Parameter::Identifier("label".to_string()),
+                                        Parameter::Integer(0x1000),
+                                      ]))),
+            //(".print $num",           Ok((Command::Print,vec![Parameter::Integer(0x10000)]))),
+        ];
+
+        for pair in pairs {
+            assert_eq!(parser.parse_cmd(pair.0), Some(pair.1));
+        }
+    }
+
+    #[test]
+    fn test_parse_token() {
+
+    }
+
 }
